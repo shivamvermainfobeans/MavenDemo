508731dc029669feebfc87315341e14c17cdc683
shivamvermainfobeans <shivam.verma@infobeans.com>
2016-04-26 17:06:38 +0530

    Added files via upload

---
+++ src/main/resources/com/maven/Application.java
@@ -0,0 +1,11 @@
+package com.maven;
+
+public class Application {
+	
+	public Application(){
+		System.out.println("Hello World!----+++");
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World!--------");
+	}
+}
